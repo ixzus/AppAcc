1316a029967269a2b2c2dffba5698496942e44aa
ixzus <iandroid@foxmail.com>
2017-12-08 17:13:41 +0800

    update

--- awdiget/src/main/java/com/eeepay/awdiget/MainActivity.java
+++ awdiget/src/main/java/com/eeepay/awdiget/MainActivity.java
@@ -3,17 +3,36 @@ package com.eeepay.awdiget;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.eeepay.awdiget.hencoder.Practice1Activity;
 import com.eeepay.awdiget.hencoder.Practice2Activity;
+import com.eeepay.awdiget.hencoder.Practice3Activity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        startActivity(new Intent(this, Practice1Activity.class));
-        startActivity(new Intent(this, Practice2Activity.class));
+        findViewById(R.id.pratice1).setOnClickListener(this);
+        findViewById(R.id.pratice2).setOnClickListener(this);
+        findViewById(R.id.pratice3).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.pratice1:
+                startActivity(new Intent(this, Practice1Activity.class));
+                break;
+            case R.id.pratice2:
+                startActivity(new Intent(this, Practice2Activity.class));
+                break;
+            case R.id.pratice3:
+                startActivity(new Intent(this, Practice3Activity.class));
+                break;
+        }
     }
 }
---
+++ awdiget/src/main/java/com/eeepay/awdiget/hencoder/Practice3Activity.java
@@ -0,0 +1,15 @@
+package com.eeepay.awdiget.hencoder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.eeepay.awdiget.R;
+
+public class Practice3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice3);
+    }
+}
